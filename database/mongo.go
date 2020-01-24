8fb22bf4bc537193886eef708911c750e8c76c81
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2020-01-25 01:20:30 +0700

    add timeout option

--- database/mongo.go
+++ database/mongo.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,6 +13,9 @@ var ctx = context.Background()
 func Connect() (*mongo.Database, error) {
 	clientOptions := options.Client()
 	clientOptions.ApplyURI("mongodb://127.0.0.1:27017/")
+	clientOptions.SetConnectTimeout(time.Second * 15)
+	clientOptions.SetServerSelectionTimeout(time.Second * 15)
+	clientOptions.SetSocketTimeout(time.Second * 15)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		return nil, err

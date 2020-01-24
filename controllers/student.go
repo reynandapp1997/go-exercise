15f761bda3c895ea4b1393f169d8ab2635d20540
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2020-01-25 01:25:30 +0700

    implement dry

--- controllers/student.go
+++ controllers/student.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"go.mongodb.org/mongo-driver/mongo"
+
 	"github.com/gorilla/mux"
 
 	"crud-golang-mongodb/database"
@@ -15,27 +17,29 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func HandleUsersList(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func getDb(w http.ResponseWriter) *mongo.Database {
 	dbase, err := database.Connect()
 	if err != nil {
-		handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		handler.ResponseHandler(w, err.Error(), http.StatusInternalServerError)
+		return nil
 	}
+	return dbase
+}
 
-	res, err := dbase.Collection("student").Find(database.GetContext(), bson.M{})
+func HandleUsersList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	res, err := getDb(w).Collection("student").Find(database.GetContext(), bson.M{})
 	if err != nil {
-		handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
+		handler.ResponseHandler(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer res.Close(database.GetContext())
-
 	result := make([]models.Student, 0)
 	for res.Next(database.GetContext()) {
 		var row models.Student
 		err := res.Decode(&row)
 		if err != nil {
-			handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
+			handler.ResponseHandler(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		result = append(result, row)
@@ -47,15 +51,10 @@ func HandleUsersList(w http.ResponseWriter, r *http.Request) {
 func HandleUserSingle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
-	dbase, err := database.Connect()
-	if err != nil {
-		handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
 	userid, _ := primitive.ObjectIDFromHex(vars["id"])
-	res := dbase.Collection("student").FindOne(database.GetContext(), bson.M{"_id": userid})
+	res := getDb(w).Collection("student").FindOne(database.GetContext(), bson.M{"_id": userid})
 	var student models.Student
-	err = res.Decode(&student)
+	err := res.Decode(&student)
 	if err != nil {
 		handler.ResponseHandler(w, "Student not found", http.StatusNotFound)
 		return
@@ -66,17 +65,12 @@ func HandleUserSingle(w http.ResponseWriter, r *http.Request) {
 
 func HandleInsertUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	dbase, err := database.Connect()
-	if err != nil {
-		handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
 	name := r.FormValue("name")
 	grade, _ := strconv.Atoi(r.FormValue("grade"))
 	student := models.Student{Name: name, Grade: grade}
-	_, err = dbase.Collection("student").InsertOne(database.GetContext(), student)
+	_, err := getDb(w).Collection("student").InsertOne(database.GetContext(), student)
 	if err != nil {
-		handler.ResponseHandler(w, "Internal Server Error", http.StatusInternalServerError)
+		handler.ResponseHandler(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	handler.ResponseHandler(w, "Success insert student", http.StatusCreated)
